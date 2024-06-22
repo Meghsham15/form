98f4162ce53187e196fbdcd1e26a9aca8e8116a7
Meghsham15 <88832001+Meghsham15@users.noreply.github.com>
2024-06-22 10:39:56 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import EventRegistrationForm from './components/EventRegistrationForm';
+import SurveyForm from './components/SurveyForm';
+import JobApplicationForm from './components/JobApplicationForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <nav>
+      <ul>
+        <li><Link to='/'> Level 1 </Link></li>
+        <li><Link to='/level2'> Level 2 </Link></li>
+        <li><Link to='/level3'> Level 3 </Link></li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route
+      path="/"
+      element={<EventRegistrationForm/>}
+      />
+      <Route
+      path="/level3"
+      element={<SurveyForm/>}
+      />
+      <Route
+      path="/level2"
+      element={<JobApplicationForm/>}
+      />
+    </Routes>
+
+    </Router>
   );
 }
 
